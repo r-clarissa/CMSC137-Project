3ebc7bef42eaadeee5821af848c2f0ead81dfbd3
slimshady616 <arcastillo1@up.edu.ph>
2023-05-30 02:06:10 +0800

    feat: implemented in game chat (but buggy, and only in console)

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,5 +1,10 @@
 package app;
 
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
 // Import statements
 import javafx.application.Application;
 import javafx.stage.Stage;
@@ -7,15 +12,28 @@ import stages.TitleScreen;
 
 public class Main extends Application {
 
-	public static void main(String[] args) {
-		launch(args);
-	}
+
 
 	@Override
 	public void start(Stage stage) throws Exception {
 
+
 		TitleScreen titleScreen = new TitleScreen();
 		titleScreen.setStage(stage);
+		@SuppressWarnings("resource")
+		Scanner scanner= new Scanner(System.in);
+		System.out.println("Enter your username for the group chat: ");
+		String username= scanner.nextLine();
+		Socket socket= new Socket("localhost",9992);
+		Client client = new Client(socket, username);
+		client.listenForMessage();
+		client.sendMessage();
+
+
+	}
+	public static void main(String[] args) throws UnknownHostException, IOException {
+		launch(args);
+
 	}
 
 }
